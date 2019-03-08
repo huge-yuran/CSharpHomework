fe5c098571d7b5e51388cffe6bb4060666c252cc
huge-yuran <45586275+huge-yuran@users.noreply.github.com>
2019-03-08 19:28:15 +0800

    Add files via upload

---
+++ homework1/1-6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Prime
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("输入一个数：");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int j = 2; j < num; j++)
+                    {
+                        while (num != j)
+                        {
+                            if (num % j == 0)
+                            {
+                                Console.Write(j + "*");
+                                num /= j;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.WriteLine(num);
+         }
+    }
+}
---
+++ homework1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Arrays
+{
+    public static void Main(string [] args)
+    {
+        Console.WriteLine(2);
+        for (int n = 3; n < 101; n++)
+        {
+            int c = 2;
+            int x = 0;
+            while (n > c)
+            {
+                if (n % c == 0)
+                {
+                    x++;
+                    break;
+                }
+                else
+                {
+                    c++;
+                }
+            }
+            if(x == 0)
+                Console.WriteLine(n);
+        }
+
+
+
+        Console.ReadKey();
+    }
+}
