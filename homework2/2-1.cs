3d1956190b9c08ddca598ab7c2d1d12a83d6ff0e
huge-yuran <45586275+huge-yuran@users.noreply.github.com>
2019-03-14 17:20:35 +0800

    Add files via upload

---
+++ homework2/2-1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1
+{
+    public interface Log
+    {
+        void Write();
+    }
+
+    class File : Log
+    {
+        public void Write()
+        {
+            Console.WriteLine("文件-日志");
+        }
+    }
+
+    class Database : Log
+    {
+        public void Write()
+        {
+            Console.WriteLine("数据库-日志");
+        }
+    }
+
+    public interface Factory
+    {
+        Log CreateLog();
+    }
+
+    class FileLogFactory : Factory
+    {
+        public Log CreateLog()
+        {
+            Log log = new File();
+            return log;
+        }
+    }
+
+    class DatabaseLogFactory : Factory
+    {
+        public Log CreateLog()
+        {
+            Log log = new Database();
+            return log;
+        }
+    }
+
+    class Client
+    {
+        public static void Main(String[] args)
+        {
+            Factory factory;
+            Log log;
+            factory = new FileLogFactory();
+            log = factory.CreateLog();
+            log.Write();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homework2/2-2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+
+    public interface IOperate               //前面加“I”为了解决命名冲突问题
+    {
+        void Estabish();
+        void Delete();
+    }
+
+    class Bussiness1 : IOperate
+    {
+        void IOperate.Estabish()
+        {
+            Console.WriteLine("+1");
+        }
+
+        void IOperate.Delete()
+        {
+            Console.WriteLine("-1");
+        }
+    }
+
+    class Bussiness2 : IOperate
+    {
+        void IOperate.Estabish()
+        {
+            Console.WriteLine("+2");
+        }
+
+        void IOperate.Delete()
+        {
+            Console.WriteLine("-2");
+        }
+    }
+
+    interface IEstabishFactory              //前面加“I”为了解决命名冲突问题
+    {
+        IOperate EstabishFigure();
+    }
+
+    class Bussiness1Factory : IEstabishFactory
+    {
+        IOperate IEstabishFactory.EstabishFigure()
+        {
+            return new Bussiness1();
+        }
+
+    }
+
+
+    class Bussiness2Factory : IEstabishFactory
+    {
+        IOperate IEstabishFactory.EstabishFigure()
+        {
+            return new Bussiness2();
+        }
+
+    }
+
+    class AllClass
+    {
+        public static void Main(string[] args)
+        {
+            IEstabishFactory Factories;
+            Factories = new Bussiness1Factory();
+            IOperate Creates = Factories.EstabishFigure();
+            Creates.Estabish();
+        }
+    }
+
+}
