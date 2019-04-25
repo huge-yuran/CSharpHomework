0f78cce01feb785528d70536d35c272777f11c84
huge-yuran <45586275+huge-yuran@users.noreply.github.com>
2019-04-25 23:21:11 +0800

    Update Program.cs

--- homework8/Order/Program.cs
+++ homework8/Order/Program.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace project1 
+namespace Order 
 {
 
     public class Program
